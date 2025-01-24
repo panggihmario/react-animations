55b21c8cc61de59193d0b630518b0bece9498a02
panggih mario <panggihmario@gmail.com>
2025-01-24 08:55:47 +0700

    add animation on modal

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import WelcomePage from './pages/Welcome.jsx';
 import ChallengesPage from './pages/Challenges.jsx';
+import Motion from './pages/Motion.jsx';
 
 const router = createBrowserRouter([
   { path: '/', element: <WelcomePage /> },
   { path: '/challenges', element: <ChallengesPage /> },
+  { path: '/motion', element: <Motion /> }
 ]);
 
 function App() {
